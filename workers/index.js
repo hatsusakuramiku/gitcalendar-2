64ae218cd742beddf0d9b7b90fcc52955d4d5fad
hatsusakuramiku <112763271+hatsusakuramiku@users.noreply.github.com>
2023-02-23 02:53:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ workers/index.js
@@ -0,0 +1,90 @@
+import { Router } from 'worktop';
+import { reply } from 'worktop/response';
+import { listen } from 'worktop/cfw';
+
+const API = new Router();
+
+API.add('GET', '/user/:username', async (_req, context) => {
+    const requestedUser = String(context.params.username);
+    const limit = Number(context.url.searchParams.get('limit') ?? -1);
+    const year = Number(context.url.searchParams.get('year') ?? -1);
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'public, max-age=86400, immutable',
+        'Access-Control-Allow-Origin': '*',
+    };
+
+    const userData = await getUserData(requestedUser, year, limit);
+
+    if (!userData.total && !userData.contributions)
+        return reply(404, JSON.stringify({ message: 'User does not exist' }), headers);
+
+    return reply(200, JSON.stringify(userData), headers);
+});
+
+listen(API.run);
+
+/**
+ * @typedef Contribution
+ * @property {string} date
+ * @property {number} count
+ * @property {(0 | 1 | 2 | 3 | 4)} intensity
+ */
+
+/**
+ * @param {string} requestedUser
+ * @param {number} [limit]
+ * @returns {Promise<{ total: number, contributions: Contribution[][] }>}
+ */
+async function getUserData(requestedUser, year, limit) {
+    // While GitHub's API seeming supports and uses both `from` and `to` parameters,
+    // the data returned is always the year included in the `to` parameter.
+    //
+    // For example, if we use `?from=2020-09-01&to=2021-01-30`, we get data for the
+    // full year of 2021.
+    const response = await fetch(
+        `https://github.com/users/${requestedUser}/contributions?to=${year}-01-01`,
+    );
+
+    let total;
+    let dayIndex = 0;
+    let contributions = [];
+
+    // @ts-ignore
+    await new HTMLRewriter()
+        .on('.js-yearly-contributions h2', {
+            /**
+             * @param {Object} text
+             * @param {string} text.text
+             * @param {boolean} text.lastInTextNode - Last chunk of the text node is always(?) empty
+             */
+            text(text) {
+                if (text.lastInTextNode) return;
+                total = parseInt(text.text.match(/[0-9,]+/)[0].replace(/,/g, ''), 10);
+            },
+        })
+        .on('g > .ContributionCalendar-day', {
+            /**
+             * @param {Element} element
+             */
+            element(element) {
+                const weekIndex = Math.floor(dayIndex / 7);
+                if (weekIndex === limit) return;
+                if (!contributions[weekIndex]) contributions.push([]);
+                contributions[weekIndex].push({
+                    date: element.getAttribute('data-date'),
+                    count: parseInt(element.getAttribute('data-count'), 10),
+                    intensity: element.getAttribute('data-level'),
+                });
+                dayIndex++;
+            },
+        })
+        .transform(response)
+        .arrayBuffer();
+
+    return {
+        total,
+        contributions,
+    };
+}
